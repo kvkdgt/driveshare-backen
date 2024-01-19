3114d10e2ddba332abc7ae8283ab4b42d07676ec
riddhielsnerr2102 <riddhi@elsner.in>
2024-01-19 11:54:06 +0530

    api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/auth/signup', [App\Http\Controllers\AuthController::class, 'Signup']);
+Route::post('/auth/login', [App\Http\Controllers\AuthController::class, 'SignIn']);
+Route::get('/auth/logout', [App\Http\Controllers\AuthController::class, 'SignOut']);
+Route::post('/auth/changepassword', [App\Http\Controllers\AuthController::class, 'ChangePassword']);
+
+
